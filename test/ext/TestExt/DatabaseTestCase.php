02f8d9d6795266fbb14ee9db0e7a9b0f2601caed
Egor V. Derevyankin <egor@nullptr.cc>
2016-05-27 11:22:13 +0300

    * database tests improvement: detect database availability

--- test/ext/TestExt/DatabaseTestCase.php
+++ test/ext/TestExt/DatabaseTestCase.php
@@ -9,7 +9,7 @@ abstract class DatabaseTestCase extends \PHPUnit_Extensions_Database_TestCase
     private $conn;
     private $sql_driver;
 
-    final protected function getDbOpts()
+    private function getDbOpts()
     {
         $opts = parse_ini_file(DBOPTS_PATH . '/connection.ini');
         $opts['dsn'] = sprintf('mysql:host=%s;dbname=%s;charset=utf8', $opts['host'], $opts['dbname']);
@@ -29,21 +29,34 @@ abstract class DatabaseTestCase extends \PHPUnit_Extensions_Database_TestCase
         return $this->sql_driver;
     }
 
-    final public function getConnection()
+    final protected function getConnection()
     {
         if ($this->conn !== null) {
             return $this->conn;
         };
 
-        $opts = $this->getDbOpts();
-        $pdo = new \PDO($opts['dsn'], $opts['username'], $opts['password']);
-        $this->conn = $this->createDefaultDBConnection($pdo, $opts['dbname']);
+        try {
+            $opts = $this->getDbOpts();
+            $pdo = new \PDO($opts['dsn'], $opts['username'], $opts['password']);
+            $this->conn = $this->createDefaultDBConnection($pdo, $opts['dbname']);
+        } catch (\Exception $ex) {
+            $this->conn = null;
+        };
 
         return $this->conn;
     }
 
-    public function getDataSet()
+    protected function getDataSet()
     {
         return $this->createMySQLXMLDataSet(DBFIXT_PATH . '/whole-dump.xml');
+    }
+
+    protected function setUp()
+    {
+        if (!$this->getConnection()) {
+            $this->markTestSkipped('Database connection not available');
+        };
+
+        return parent::setUp();
     }
 }
